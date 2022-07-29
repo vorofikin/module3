59d7da30d6d9a461d6bf105fe03f788b80dc182d
vorofikin <nikiforov564@gmail.com>
2022-07-29 12:59:02 +0300

    my first commit

---
+++ Homework_10.js
@@ -0,0 +1,35 @@
+function CarName (name){
+    this.name = name;
+}
+
+const car1 = new CarName('Nissan');
+let car2 = new CarName('Porsche');
+const car3 = new CarName('Cadillac');
+
+console.log(car1);
+
+const obj = Object.create(CarName, {
+    car1: {
+        value: 'Nissan',
+        writable: false,
+        configurable: true
+    },
+    car2: {
+        value: 'Porsche',
+        writable: false,
+        configurable: true
+    },
+    car3: {
+        value: 'Cadillac'
+    }
+});
+
+
+class CarNameClass {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const carName1 = new CarNameClass('Volvo');
+const carName2 = new CarNameClass('Lamborghini');
